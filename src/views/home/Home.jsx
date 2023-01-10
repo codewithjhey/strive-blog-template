cd2cd247b1f1e0902e4c4545037e1af1553dc6e0
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2023-01-10 13:31:06 +0000

    .

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import BlogList from "../../components/blog/blog-list/BlogList";
-import "./styles.css";
+import React from "react"
+import { Container } from "react-bootstrap"
+import BlogList from "../../components/blog/blog-list/BlogList"
+import { useState, useEffect } from "react"
+import "./styles.css"
 
-const Home = (props) => {
+const Home = () => {
+  const [blogPosts, setBlogPosts] = useState([])
+
+  useEffect(() => {
+    fetchBlogPosts()
+  }, [])
+  const fetchBlogPosts = async () => {
+    try {
+      const res = await fetch(`http://localhost:3001/blogPosts`)
+      if (res.ok) {
+        const data = await res.json()
+        console.log(data)
+        setBlogPosts(data)
+      } else {
+        alert("there was an error fetching")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Container fluid="sm">
       <h1 className="blog-main-title">Welcome to the Epicode Blog!</h1>
-      <BlogList />
+      <BlogList blogPosts={blogPosts} />
     </Container>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
